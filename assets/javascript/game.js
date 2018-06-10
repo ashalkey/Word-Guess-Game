17a7c8c0f1d7855d390255527e85e4b7236f3a0b
Aldosh <alanshalkey@gmail.com>
2018-06-10 16:30:01 -0600

    finishing touches

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,115 @@
 var numWins = 0;
+var numLosses = 0;
 var numGuesses = 10;
 var lettersGuessed = [""];
-var maFilms = ["Enter The Dragon", "Police Story", "The Legend of the Drunken Master", "Crouching Tiger Hidden Dragon", "Fist of Legend", "The Protector", "Ip Man"];
-// var maFilms = ["EnterTheDragon"];
+var maFilms = ["Enter The Dragon", "Police Story", "The Legend of Drunken Master", 
+"Crouching Tiger Hidden Dragon", "Fist of Legend", "The Protector", "Ip Man",
+"Five Deadly Venoms", "The 36th Chamber of Shaolin", "Once Upon a Time in China",
+"Master of the Flying Guillotine", "Shogun Assassin", "Wheels on Meals", "Fist of Fury",
+"Rumble in the Bronx", "The 36th Chamber of Shaolin", "The Eight Diagram Pole Fighter"];
+// var maFilms = ["Enter The Dragon", "Police Story"];
 var currentFilm = maFilms[Math.floor(Math.random()*maFilms.length)].toLowerCase();
 var currentAnswer = [""];
+var youWon = false;
+var textRemoved = false;
+var answeredFilms = [''];
 
-
-function chooseFilm() {
+function parseFilm() {
 
     for (var i = 0; i < currentFilm.length; i++) {
 
-
-        currentAnswer[i] = "_";
+        if (currentFilm.charAt(i) === " ") {
+            currentAnswer[i] = " ";
+        }
+        else {
+            currentAnswer[i] = "_";
+        }
     }
 
-    document.getElementById('maFilm').innerHTML = currentAnswer.join(" ");
+    document.getElementById('maFilm').innerHTML = currentAnswer.join("");
 }
 
-chooseFilm();
+parseFilm();
 
 document.onkeyup = function (event) {
-
+    if(!youWon){
+    if (!textRemoved) {
+        document.getElementById('introText').remove();
+        textRemoved = true;
+    }
     var letterPressed = event.key;
     var idxLetterPressed = currentFilm.indexOf(letterPressed);
 
-    while(idxLetterPressed != -1){
-            currentAnswer[idxLetterPressed] = letterPressed;
-            document.getElementById('maFilm').innerHTML = currentAnswer.join(" ");
+    while (idxLetterPressed != -1) {
+        currentAnswer[idxLetterPressed] = letterPressed;
+        document.getElementById('maFilm').innerHTML = currentAnswer.join("");
 
-            idxLetterPressed = currentFilm.indexOf(letterPressed, idxLetterPressed + 1);
+        idxLetterPressed = currentFilm.indexOf(letterPressed, idxLetterPressed + 1);
 
-            if (currentAnswer.join("") === currentFilm) {
-                numWins++;
-                document.getElementById('wins').innerHTML = numWins;
-                var audio = document.getElementById('audio');
-                audio.play();
-                document.getElementById('maFilm').innerHTML += "<br> That is CORRECT! <br> Press enter to play again";
+        if (currentAnswer.join("") === currentFilm) {
+            youWon = true;
+            numWins++;
+            document.getElementById('wins').innerHTML = numWins;
+            var audio = document.getElementById('audio');
+            audio.play();
+            document.getElementById('winText').innerHTML = "That is CORRECT! <br> Press enter to play again";
+            if (answeredFilms[0] === ""){
+            answeredFilms[0] = currentFilm;
             }
+            else {
+                answeredFilms.push(currentFilm);
+            }
+            document.getElementById('answeredFilms').innerHTML = answeredFilms.join(", ");
         }
+    }
 
-        if (!currentFilm.includes(letterPressed) && !lettersGuessed.includes(letterPressed)) {
-            console.log("Letter pressed is " + letterPressed);
-            numGuesses--;
-            document.getElementById('guesses').innerHTML = numGuesses;
-            lettersGuessed.push(letterPressed);
-            document.getElementById('lettersGuessed').innerHTML = lettersGuessed.join(" ");
-        }
-    if (event.key === "Enter" && numWins > 0) {
+    if (!currentFilm.includes(letterPressed) && !lettersGuessed.includes(letterPressed)) {
+        numGuesses--;
+        document.getElementById('guesses').innerHTML = numGuesses;
+        lettersGuessed.push(letterPressed);
+        document.getElementById('lettersGuessed').innerHTML = lettersGuessed.join(" ");
+    }
+}
+    if (event.key === "Enter" && youWon === true) {
         reset();
+    }
+
+    if (numGuesses === 0) {
+        alert("You lost! Resetting the game");
+        numLosses++;
+        document.getElementById('losses').innerHTML = numLosses;
+        reset();
+    }
+
+}
+
+function chooseFilm() {
+
+    currentFilm = maFilms[Math.floor(Math.random() * maFilms.length)].toLowerCase();
+
+    while (answeredFilms.includes(currentFilm)) {
+
+        if (answeredFilms.length === maFilms.length) {
+            document.getElementById('gotEmAll').innerHTML = "You've guessed all of the films correctly! Congratulations!";
+            break;
+        }
+        else {
+        currentFilm = maFilms[Math.floor(Math.random() * maFilms.length)].toLowerCase();
+        }
     }
 }
 
-
 function reset() {
-    var numGuesses = 10;
+    youWon = false;
+    document.getElementById('winText').innerHTML = "";
+    numGuesses = 10;
     document.getElementById('guesses').innerHTML = numGuesses;
-    currentFilm = maFilms[Math.floor(Math.random()*maFilms.length)].toLowerCase();
-    document.getElementById('maFilm').innerHTML = "";
     chooseFilm();
-
+    currentAnswer = [""];
+    lettersGuessed = [""];
+    document.getElementById('lettersGuessed').innerHTML = lettersGuessed;
+    parseFilm();
 }
+
 
 
